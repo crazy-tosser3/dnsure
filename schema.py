f1e05b9c12df76253b6e9a1db8b4c17f2d67fd78
crzTosser3 <alexeyrnd28@yandex.ru>
2026-06-17 16:45:49 +0300

    conflict solved

--- schema.py
+++ schema.py
@@ -1,19 +1,4 @@
 from pydantic import BaseModel
-<<<<<<< HEAD
-
-class ConnInfo(BaseModel):
-    host: str
-    port: int
-    password: str
-
-
-class CheckInfo(BaseModel):
-    host: str
-    check_type: list
-    server_location: str
-
-
-=======
 from typing import List
 
 class ConnInfo(BaseModel):
@@ -27,5 +12,4 @@ class CheckInfo(BaseModel):
     host_to_check: str
     check_type: List[str]
     agents_location: str
->>>>>>> 416e287 (add functionality)
 
